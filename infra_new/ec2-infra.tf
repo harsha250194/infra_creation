f2a72dec3bf38680539dedca05f9caaff0c140c4
harsha.yelamanchili <harsha.yelamanchili@toyota.com>
2021-09-15 13:50:22 -0600

    rectifications

--- infra_new/ec2-infra.tf
+++ infra_new/ec2-infra.tf
@@ -90,7 +90,7 @@ resource "aws_instance" "ec2_public" {
     inline = [
       "chmod 400 ~/${var.key_name}.pem",
       "export DD_AGENT_MAJOR_VERSION=7",
-      "export DD_API_KEY=${var.ddapikey}",
+      "export DD_API_KEY='${var.ddapikey}'",
       "export DD_SITE='datadoghq.com'",
       "curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script.sh",
       "bash -c install_script.sh"
